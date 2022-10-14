322a5115b34a0822f1b18c09fb4369e8d09cf520
Rauane Lima <rauanelima7@gmail.com>
2022-10-14 12:21:10 -0300

    coding the page App

--- src/Components/button.tsx
+++ src/Components/button.tsx
@@ -1,20 +1,24 @@
-import clsx from 'clsx';
-import {Slot} from '@radix-ui/react-slot'
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
 
-export interface ButtonProps{
-    Children: ReactNode;
-    asChild?: boolean; 
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  asChild?: boolean;
 }
 
-export function Button({ Children, asChild}: ButtonProps){
-    const Comp = asChild ? Slot : 'button';
+export function Button({ children, asChild, className, ...props }: ButtonProps) {
+  const Comp = asChild ? Slot : 'button';
 
-    return(
-        <Comp 
-        className={clsx('py-4 px-3 bg-cyan-500 trasition-colors focus:ring-2 ring-white rounded font-semibold text-black text-sm w-full hover:bg-cyan-300')}
-        >
-        {Children}
-        </Comp>
-    )
+  return (
+    <Comp 
+      className={clsx(
+        'py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </Comp>
+  )
 }
--- src/Components/heading.tsx
+++ src/Components/heading.tsx
@@ -1,26 +1,30 @@
-import clsx from 'clsx';
-import {Slot} from '@radix-ui/react-slot'
 import { ReactNode } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
 
-export interface HeadingProps{
-    size?: 'sm' | 'md' | 'lg';
-    Children: ReactNode;
-    asChild?: boolean; 
+export interface HeadingProps {
+  size?: 'sm' | 'md' | 'lg';
+  children: ReactNode;
+  asChild?: boolean;
+  className?: string;
 }
 
-export function Heading({size = 'md', Children, asChild}: HeadingProps){
-    const Comp = asChild ? Slot : 'h2';
+export function Heading({ size = 'md', children, asChild, className }: HeadingProps) {
+  const Comp = asChild ? Slot : 'h2';
 
-    return(
-        <Comp 
-        className={clsx('text-gray-100 font-bold font-sans',{
-        'text-lg' : size === 'sm',
-         'text-xl' : size === 'md',
-         'text-2xl' : size === 'lg',   
-        }
-        )}
-        >
-        {Children}
-        </Comp>
-    )
+  return (
+    <Comp 
+      className={clsx(
+        'text-gray-100 font-bold font-sans', 
+        {
+          'text-lg': size === 'sm',
+          'text-xl': size === 'md',
+          'text-2xl': size === 'lg',
+        },
+        className
+      )}
+    >
+      {children}
+    </Comp>
+  )
 }
